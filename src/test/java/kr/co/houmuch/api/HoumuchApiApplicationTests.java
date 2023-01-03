cc18ee79a0a29e7cf87aa4d5fedcabe4fc41bc34
chaehun <chaehun@uniess.co.kr>
2023-01-03 17:25:24 +0900

    최초 커밋

---
+++ src/test/java/kr/co/houmuch/api/HoumuchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.houmuch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoumuchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
