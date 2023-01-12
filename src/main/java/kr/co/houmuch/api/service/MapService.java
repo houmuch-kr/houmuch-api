eebcb3da9b79b6a97eb1a48273384b8f42fd827b
hyodii <hyodii9736@gmail.com>
2023-01-12 11:15:09 +0900

    remove : 불필요 코드 제거

--- src/main/java/kr/co/houmuch/api/service/MapService.java
+++ src/main/java/kr/co/houmuch/api/service/MapService.java
@@ -1,16 +1,8 @@
 package kr.co.houmuch.api.service;
 
-import kr.co.houmuch.core.domain.code.AreaCodeJpo;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class MapService {
-//    @Transactional(readOnly = true)
-//    public Page<AreaCodeJpo> fetch() {
-//        return findSidoAll(pageable);
-//    }
+
 }
