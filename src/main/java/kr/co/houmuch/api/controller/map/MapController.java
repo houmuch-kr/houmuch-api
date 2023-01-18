fa738ef4307a682db30e1d1424b65d3caadc401b
hyodii <hyodii9736@gmail.com>
2023-01-18 16:43:20 +0900

    fix : 컨트롤러에 ResponseBody 제거

--- src/main/java/kr/co/houmuch/api/controller/map/MapController.java
+++ src/main/java/kr/co/houmuch/api/controller/map/MapController.java
@@ -4,16 +4,12 @@ import io.swagger.annotations.Api;
 import kr.co.houmuch.api.service.MapService;
 import kr.co.houmuch.api.swagger.SwaggerApiInfo;
 import kr.co.houmuch.core.domain.code.AreaCodeJpaRepository;
-import kr.co.houmuch.core.domain.code.AreaCodeJpo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@ResponseBody
 @RequestMapping(path = "/v1/map")
 @RequiredArgsConstructor
 @Api(tags = SwaggerApiInfo.MAP_LIST)
@@ -21,7 +17,7 @@ public class MapController {
     private final AreaCodeJpaRepository areaCodeJpaRepository;
     private final MapService mapService;
     @GetMapping(path = "/fetchList")
-    public List<?> fetchList(@RequestParam(name = "type", required = false, defaultValue = "5") int type){
+    public List<?> fetchList(@RequestParam(name = "type", required = false, defaultValue = "2") int type){
         return mapService.fetchAsync(type);
     }
 }
