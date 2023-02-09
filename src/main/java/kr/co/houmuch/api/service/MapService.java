d9405b756d8e696be4bc3a17100e628c371254c4
hyodii <hyodii9736@gmail.com>
2023-02-09 21:30:22 +0900

    fix : fetch 메소드 case문 사용 및 불필요한 코드 제거

--- src/main/java/kr/co/houmuch/api/service/MapService.java
+++ src/main/java/kr/co/houmuch/api/service/MapService.java
@@ -1,10 +1,11 @@
 package kr.co.houmuch.api.service;
 
 import kr.co.houmuch.api.domain.dto.map.AreaContract;
+import kr.co.houmuch.core.domain.building.jpa.BuildingJpaRepository;
 import kr.co.houmuch.core.domain.code.AreaCodeJpaRepository;
 import kr.co.houmuch.core.domain.code.AreaCodeJpo;
 import kr.co.houmuch.core.domain.code.dto.AreaCode;
-import kr.co.houmuch.core.domain.contract.dto.Contract;
+import kr.co.houmuch.core.domain.common.jpa.CombinedAreaCodeJpo;
 import kr.co.houmuch.core.domain.contract.jpa.ContractDetailJpo;
 import kr.co.houmuch.core.domain.contract.jpa.ContractJpaRepository;
 import kr.co.houmuch.core.domain.contract.jpa.ContractJpo;
@@ -18,104 +19,119 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class MapService {
-		private final AreaCodeJpaRepository areaCodeJpaRepository;
-		private final ContractJpaRepository contractJpaRepository;
+public class MapService{
+    private final AreaCodeJpaRepository areaCodeJpaRepository;
+    private final ContractJpaRepository contractJpaRepository;
+    private final BuildingJpaRepository buildingJpaRepository;
 
-		public List<AreaContract> fetch(int type) {
-				List<AreaCodeJpo> areaCodeList = areaCodeJpaRepository
-								.findByType2(type);
-				List<AreaContract> areaContractList = new ArrayList<>();
-				List<ContractJpo> contractList = contractJpaRepository
-								.findByBuildingAreaCode2(areaCodeList.subList(0, 100));
-				for (ContractJpo contractJpo : contractList) {
-						AreaContract areaContract = AreaContract.builder()
-										.areaCode(AreaCode.entityOf(contractJpo.getBuilding().getAreaCode()))
-										.count(contractList.size())
-										.price(contractList.stream()
-														.map(ContractJpo::getDetail)
-														.mapToInt(ContractDetailJpo::getPrice)
-														.average()
-														.orElse(0))
-										.contracts(contractList.stream().map(Contract::entityOf).collect(Collectors.toList()))
-										.build();
-						areaContractList.add(areaContract);
-				}
-				return areaContractList;
-		}
+    public List<AreaContract> fetch(int type) {
+        List<AreaCodeJpo> areaCodeList = areaCodeJpaRepository
+                .findByType(type);
 
-		public List<AreaContract> fetchList(int type) {
-				PageRequest pageable = PageRequest.of(0, 30);
-				System.out.println("getContent----->" + areaCodeJpaRepository.findSido(pageable).getContent());
-				System.out.println("getTotalElements----->" + areaCodeJpaRepository.findSido(pageable).getTotalElements());
+        List<Integer> sidoLists = areaCodeList.stream()
+                .map(AreaCodeJpo::getCode)
+                .map(CombinedAreaCodeJpo::getSido)
+                .toList();
 
-//				List<Long> areaCodeList = areaCodeJpaRepository
-//								.findSido(pageable)
-//								.stream()
-//								.map(areaCodeJpo -> areaCodeJpo.getId())
-//								.collect(Collectors.toList());
-//				System.out.println("areaCodeList--->" + areaCodeList);
-//
+        System.out.println("areaCodeList--->" + areaCodeList);
+        System.out.println("sidoLists---->" + sidoLists);
 
-//				List<AreaCodeJpo> areaCodeList22 = areaCodeJpaRepository.findAll(pageable).getContent();
-//				System.out.println("areaCodeList22->"+areaCodeList22);
-//
-//				List<ContractJpo> findContractList = contractJpaRepository.findByBuildingAreaCode(areaCodeList22.get(0), pageable).getContent();
-//				System.out.println("findContractList----->" + findContractList);
+        // 모든 거래내역들
+        List<ContractJpo> contractList = contractJpaRepository.findByBuildingAreaCode(sidoLists);
 
+        List<AreaContract> areaContractList = new ArrayList<>();
 
-				List<AreaCodeJpo> findAreaCodeList = new ArrayList<>();
+        switch (type) {
+            case 0:
+                for (AreaCodeJpo areaCodeJpo : areaCodeList){
+                    int count = 0;
+                    double contractPrice = 0.0;
+                    for (ContractJpo contractJpo : contractList) {
+                        int contractAreaCode = contractJpo.getBuilding().getAreaCode().getCode().getSido();
+                        int areaCodeSido = areaCodeJpo.getCode().getSido();
+                        if (contractAreaCode == (areaCodeSido)) {
+                            count++;
+                            contractPrice += contractJpo.getDetail().getPrice();
+                        }
+                    }
+                    contractPrice = contractPrice / count;
+                    System.out.println("contractPrice----->" + contractPrice);
+                    System.out.println("count---->" + count);
 
-				List<Integer> sidoList = areaCodeJpaRepository
-								.findSido(pageable)
-								.stream()
-								.map(areaCodeJpo -> areaCodeJpo.getCode().getSido())
-								.collect(Collectors.toList());
-				for (int sidoInt : sidoList
-				) {
-						findAreaCodeList.addAll(areaCodeJpaRepository.findByCodeSido(sidoInt, pageable).getContent());
-						System.out.println("findAreaCodeList----->" + findAreaCodeList);
-				}
-//    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-				List<ContractJpo> findContractList = new ArrayList<>();
-				for (AreaCodeJpo areaCodeJpo : findAreaCodeList
-				) {
-						findContractList.addAll(contractJpaRepository.findByBuildingAreaCode(areaCodeJpo, pageable).getContent());
-						System.out.println("findContractList---->" + findContractList);
-				}
+                    AreaContract areaContract = AreaContract.builder()
+                            .areaCode(AreaCode.entityOf(areaCodeJpo))
+                            .count(count)
+                            .price(contractPrice)
+                            .build();
+                    areaContractList.add(areaContract);
+                }
+                break;
 
-//				List<Integer> sidoList = areaCodeJpaRepository
-//								.findSido(pageable)
-//								.stream()
-//								.map(areaCodeJpo -> areaCodeJpo.getCode().getSido())
-//								.collect(Collectors.toList());
-//				System.out.println("sidoList---->" + sidoList);
-//
-//				List<Integer> sggList = areaCodeJpaRepository
-//								.findSgg(pageable)
-//								.stream()
-//								.map(areaCodeJpo -> areaCodeJpo.getCode().getSgg())
-//								.collect(Collectors.toList());
-//				System.out.println("sggList---->" + sggList);
-//
-//				List<AreaCodeJpo> findAreaCodeList = new ArrayList<>();
-//
-//				for (int sidoInt : sidoList) {
-////						System.out.println("sidoInt--->" + sidoInt);
-//						System.out.println("00000------>" + buildingJpaRepository.findById(sidoList));
-//						for (int sggInt : sggList) {
-//								findAreaCodeList.addAll(areaCodeJpaRepository.findByCodeSidoAndCodeSgg(sidoInt, sggInt, pageable).getContent());
-////								System.out.println("뭐나올랑가?----->" + findAreaCodeList);
-//						}
-//				}
-//
-//				List<ContractJpo> findConstractList = new ArrayList<>();
-//				findConstractList = contractJpaRepository.findById(pageable);
-//				contractJpaRepository.findAll(findAreaCodeList, pageable);
-//				System.out.println("findConstractList---->" + contractJpaRepository.findAll(findAreaCodeList, pageable));
+            case 1:
+                for (AreaCodeJpo areaCodeJpo : areaCodeList){
+                    AreaContract areaContract = AreaContract.builder()
+                            .areaCode(AreaCode.entityOf(areaCodeJpo))
+//                            .count(contractList.size() == 0 ? contractList
+//                                    .stream()
+//                                    .collect(Collectors.groupingBy(contractJpo -> contractJpo.getBuilding().getAreaCode().getCode().getSgg()))
+//                                    .get(areaCodeJpo.getCode().getSgg())
+//                                    .size() : 0
+//                            )
+//                            .price(contractList
+//                                    .stream()
+//                                    .collect(Collectors.groupingBy(contractJpo -> contractJpo.getBuilding().getAreaCode().getCode().getSgg()))
+//                                    .get(areaCodeJpo.getCode().getSgg())
+//                                    .stream()
+//                                    .map(ContractJpo::getDetail)
+//                                    .mapToInt(ContractDetailJpo::getPrice)
+//                                    .average()
+//                                    .orElse(0)
+//                            )
+                            .build();
+                    areaContractList.add(areaContract);
+                }
 
-				List<AreaContract> areaList = findContractList.stream().map(AreaContract::entityOf).collect(Collectors.toList());
-				return areaList;
-//				return null;
-		}
+                break;
+
+            case 2:
+                for (ContractJpo contractJpo : contractList) {
+                    AreaContract areaContract = AreaContract.builder()
+                            .areaCode(AreaCode.entityOf(contractJpo.getBuilding().getAreaCode()))
+//            .count(contractList.size())
+                            .count(contractList
+                                    .stream()
+                                    .collect(Collectors.groupingBy(contractJpo1 -> contractJpo1.getBuilding().getId()))
+                                    .get(contractJpo.getBuilding().getId())
+
+                                    .size()
+                            )
+//            .price(contractList.stream()
+//                    .map(ContractJpo::getDetail)
+//                    .mapToInt(ContractDetailJpo::getPrice)
+//                    .average()
+//                    .orElse(0))
+//            .contracts(contractList.stream().map(Contract::entityOf).collect(Collectors.toList()))
+                            .price(contractList
+                                    .stream()
+                                    .collect(Collectors.groupingBy(contractJpo1 -> contractJpo1.getBuilding().getId()))
+                                    .get(contractJpo.getBuilding().getId())
+                                    .stream()
+                                    .map(ContractJpo::getDetail)
+                                    .mapToInt(ContractDetailJpo::getPrice)
+                                    .average()
+                                    .orElse(0)
+                            )
+//                            .building(contractJpo.getBuilding().getCoordinate() != null ?
+//                                    Building.of(contractJpo.getBuilding().getName()
+//                                            , Coordinate.of(contractJpo.getBuilding().getCoordinate().getCoordinate().getLatitude()
+//                                                    , contractJpo.getBuilding().getCoordinate().getCoordinate().getLongitude()))
+//                                    : null)
+                            .build();
+                    areaContractList.add(areaContract);
+                }
+                break;
+        }
+
+        return areaContractList;
+    }
 }
