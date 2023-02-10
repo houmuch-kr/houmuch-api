b8a56212dbbbb68a59e1ff10eb2b3535cc7bc4ad
hyodii <hyodii9736@gmail.com>
2023-02-10 15:25:04 +0900

    fix : 피드백 참고 후 수정 1. 제네릭 타입 명확히 선언 2. 직전 레이어 참조하도록 Repository 코드 제거 3. 불필요한 코드 제거

--- src/main/java/kr/co/houmuch/api/controller/map/MapController.java
+++ src/main/java/kr/co/houmuch/api/controller/map/MapController.java
@@ -1,28 +1,24 @@
 package kr.co.houmuch.api.controller.map;
 
 import io.swagger.annotations.Api;
+import kr.co.houmuch.api.domain.dto.map.AreaContract;
 import kr.co.houmuch.api.service.MapService;
 import kr.co.houmuch.api.swagger.SwaggerApiInfo;
 import kr.co.houmuch.core.domain.code.AreaCodeJpaRepository;
-import kr.co.houmuch.core.domain.code.AreaCodeJpo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@ResponseBody
 @RequestMapping(path = "/v1/map")
 @RequiredArgsConstructor
 @Api(tags = SwaggerApiInfo.MAP_LIST)
 @CrossOrigin(origins = "*", methods = RequestMethod.GET)
 public class MapController {
-    private final AreaCodeJpaRepository areaCodeJpaRepository;
     private final MapService mapService;
     @GetMapping(path = "/fetchList")
-    public List<?> fetchList(@RequestParam(name = "type", required = false, defaultValue = "0") int type){
+    public List<AreaContract> fetchList(@RequestParam(name = "type", required = false, defaultValue = "0") int type){
         return mapService.fetch(type);
     }
 }
