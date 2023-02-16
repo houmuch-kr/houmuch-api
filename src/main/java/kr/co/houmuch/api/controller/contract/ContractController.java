4f02bd3062426408957bc71d4cca9d905a930325
chaehun <chaehun@uniess.co.kr>
2023-02-16 16:07:02 +0900

    feat : ContractController @CrossOrigin 추가

--- src/main/java/kr/co/houmuch/api/controller/contract/ContractController.java
+++ src/main/java/kr/co/houmuch/api/controller/contract/ContractController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/v1/contract")
+@CrossOrigin
 public class ContractController {
     private final ContractAreaFetchService contractAreaFetchService;
     private final ContractBuildingFetchService contractBuildingFetchService;
