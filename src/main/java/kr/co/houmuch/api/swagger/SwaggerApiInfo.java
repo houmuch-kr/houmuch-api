caa984e415e889043dc4d9c5ff206535f537bda6
hyodii <hyodii9736@gmail.com>
2023-02-20 14:31:41 +0900

    fix : SwaggerInfo final 변수명 룰 적용

--- src/main/java/kr/co/houmuch/api/swagger/SwaggerApiInfo.java
+++ src/main/java/kr/co/houmuch/api/swagger/SwaggerApiInfo.java
@@ -1,9 +1,9 @@
 package kr.co.houmuch.api.swagger;
 
 public class SwaggerApiInfo {
-    // controller 대제목
+    // controller 대제목(단어)
     public static final String CONTRACT = "CONTRACT 관련 API";
 
-    // 각 controller 설명
-    public static final String AreaContract_List = "type별 지역 거래(지역/총거래량/평균거래량 리스트)";
+    // 각 controller 설명(받아오는방식_어떤걸_받아오는지)
+    public static final String GET_AREA_CONTRACT_LIST = "type별 지역 거래(지역/총거래량/평균거래량 리스트)";
 }
