558c7edf3d52bbdaa76a9c6b0032e7ea2dea2f84
hyodii <hyodii9736@gmail.com>
2023-02-26 11:40:17 +0900

    feat : SwaggerApiInfo static 변수 추가

--- src/main/java/kr/co/houmuch/api/swagger/SwaggerApiInfo.java
+++ src/main/java/kr/co/houmuch/api/swagger/SwaggerApiInfo.java
@@ -4,6 +4,7 @@ public class SwaggerApiInfo {
     // controller 대제목(단어)
     public static final String CONTRACT = "CONTRACT 관련 API";
 
+    public static final String GET_PROCESS_INDEX = "type별 지역 거래(지역/총거래량/평균거래량 리스트) 캐싱";
     // 각 controller 설명(받아오는방식_어떤걸_받아오는지)
     public static final String GET_AREA_CONTRACT_LIST = "type별 지역 거래(지역/총거래량/평균거래량 리스트)";
 }
