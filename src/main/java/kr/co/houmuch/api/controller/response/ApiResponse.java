83590d5e31173162fec7d3cf8a4fe09db16fbf58
hyodii <hyodii9736@gmail.com>
2023-03-06 14:12:27 +0900

    fix : ContractController에 ErrorCode enum 적용

--- src/main/java/kr/co/houmuch/api/controller/response/ApiResponse.java
+++ src/main/java/kr/co/houmuch/api/controller/response/ApiResponse.java
@@ -1,6 +1,7 @@
 package kr.co.houmuch.api.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import kr.co.houmuch.api.constant.ErrorCode;
 import lombok.*;
 
 @Getter
@@ -17,8 +18,10 @@ public class ApiResponse<T> {
     public static ApiResponse<Void> empty() {
         return new ApiResponse<>();
     }
-
     public static <T> ApiResponse<T> of(T data) {
         return new ApiResponse<>(null, null, data);
+    }
+    public static <T> ApiResponse<T> failure(ErrorCode errorCode) {
+        return new ApiResponse<>(errorCode.name(), errorCode.getMessage(), null);
     }
 }
