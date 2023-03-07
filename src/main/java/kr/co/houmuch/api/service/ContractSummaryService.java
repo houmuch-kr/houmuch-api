7f8e867b009debbfde1f7b157ca62d9b23597f0b
hyodii <hyodii9736@gmail.com>
2023-03-07 11:30:22 +0900

    refactor : ContractSummaryService -> Stream 이용해서 리팩토링

--- src/main/java/kr/co/houmuch/api/service/ContractSummaryService.java
+++ src/main/java/kr/co/houmuch/api/service/ContractSummaryService.java
@@ -19,8 +19,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -36,7 +34,7 @@ public class ContractSummaryService {
     private final ContractSummaryJpaRepository contractSummaryJpaRepository;
 
     @Transactional
-    public void indexV2Stream() {
+    public void indexV2() {
         List<ContractJpo> contractList = contractJpaRepository.findByType(ContractType.TRADE);
         Map<AreaCodeJpo, List<ContractJpo>> contractMap = contractList
                 .stream()
@@ -56,51 +54,7 @@ public class ContractSummaryService {
         contractSummaryJpaRepository.saveAll(contractSummaryList);
     }
     @Transactional
-    public void indexV2() {
-        List<AreaCodeJpo> areaCodeList = areaCodeJpaRepository.findAllFetchJoin();
-        List<ContractJpo> contractList = contractJpaRepository.findByType(ContractType.TRADE);
-        Map<AreaCodeJpo, List<ContractJpo>> contractMap = new HashMap<>();
-        for (AreaCodeJpo areaCodeJpo : areaCodeList) {
-            List<ContractJpo> areaContractList = contractList
-                    .stream()
-                    .filter(contractJpo -> {
-                        AreaCodeJpo contractAreaCode = contractJpo.getBuilding().getAreaCode();
-                        return areaCodeJpo.isHierarchy(contractAreaCode);
-                    })
-                    .collect(Collectors.toList());
-            contractMap.put(areaCodeJpo, areaContractList);
-        }
-        List<ContractSummaryJpo> contractSummaryList = new ArrayList<>();
-        for (Map.Entry<AreaCodeJpo, List<ContractJpo>> entry : contractMap.entrySet()) {
-            if (entry.getValue().isEmpty()) {
-                ContractSummaryJpo contractSummary = ContractSummaryJpo.builder()
-                        .id(entry.getKey().getId())
-                        .count(0)
-                        .price(0.0)
-                        .build();
-                contractSummaryList.add(contractSummary);
-                continue;
-            }
-            int totalPrice = 0;
-            int count = entry.getValue().size();
-            for (ContractJpo contractJpo : entry.getValue()) {
-                totalPrice += contractJpo.getDetail().getPrice();
-            }
-            double price = (double) totalPrice / count;
-            ContractSummaryJpo contractSummary = ContractSummaryJpo.builder()
-                    .id(entry.getKey().getId())
-                    .count(count)
-                    .price(price)
-                    .build();
-            contractSummaryList.add(contractSummary);
-        }
-        contractSummaryJpaRepository.saveAll(contractSummaryList);
-    }
-
-    @Transactional
     public void index() {
-//        전국 area_code 조회해서
-//        price 와 count 를 알아내면 됨
         List<AreaCodeJpo> areaCodeList = areaCodeJpaRepository.findAllFetchJoin();
         for(AreaCodeJpo areaCodeJpo : areaCodeList){
             CombinedAreaCodeJpo combinedAreaCodeJpo = areaCodeJpo.getCode();
@@ -140,33 +94,21 @@ public class ContractSummaryService {
 
     public List<Summary> fetchBuilding(double maxLatitude, double minLatitude, double maxLongitude, double minLongitude) {
         List<ContractJpo> contractJpoList = buildingJpaRepository.findAllByContract(maxLatitude, minLatitude, maxLongitude, minLongitude);
-
         Map<BuildingJpo, List<ContractJpo>> buildingMap = contractJpoList
                 .stream()
                 .collect(Collectors.groupingBy(contractJpo1 -> contractJpo1.getBuilding()));
-        System.out.println("buildingMap---->" + buildingMap);
-        List<Summary> buildingSummaryList = new ArrayList<>();
-        for(Map.Entry<BuildingJpo, List<ContractJpo>> entry : buildingMap.entrySet()){
-            if(entry.getValue().isEmpty()){
-                buildingSummaryList.add(BuildingSummary.builder()
-                        .building(Building.entityOf(entry.getKey()))
-                        .price(0.0)
-                        .count(0)
-                        .build());
-                continue;
-            }
-            int totalPrice = 0;
-            int count = entry.getValue().size();
-            for(ContractJpo contractJpo : entry.getValue()){
-                totalPrice += contractJpo.getDetail().getPrice();
-            }
-            double price = (double) totalPrice / count;
-            buildingSummaryList.add(BuildingSummary.builder()
-                    .building(Building.entityOf(entry.getKey()))
-                    .price(price)
-                    .count(count)
-                    .build());
-        }
+        List<Summary> buildingSummaryList = buildingMap.entrySet()
+                    .stream()
+                    .map(entry -> BuildingSummary.builder()
+                            .building(Building.entityOf(entry.getKey()))
+                            .count(entry.getValue().size())
+                            .price(entry.getValue()
+                                    .stream()
+                                    .mapToInt(value -> value.getDetail().getPrice())
+                                    .average()
+                                    .orElse(0))
+                            .build())
+                .collect(Collectors.toList());
         return buildingSummaryList;
     }
 }
